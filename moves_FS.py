6f8886a472dd2e226268343c99dd36d664c2ebd5
Josua Kugler <josua.kugler@gmail.com>
2019-09-16 14:43:08 +0200

    added comparehs function
    function that can compare the history of several THobjects passed to it in a list

--- moves_FS.py
+++ moves_FS.py
@@ -12,64 +12,88 @@ success_instances = [] # a list with all instances that reached the finished sta
 def movessequence(startpeg, endpeg, disklist, peglist):
     """gibt alle benoetigten Zuege zurueck"""
     print("Die Scheiben {} sollen unter Benutzung der Felder {} von {} nach {} bewegt werden".format(disklist,peglist,startpeg,endpeg))
-    #print(disklist)
-    #spezialfaelle
+    #rint(disklist)
+    #if there is only one disk left, there is only one possible move
     if len(disklist)==1:
-        #print("es bleibt nur eine Scheibe zum bewegen",disklist[0],startpeg,endpeg)
-        return [[disklist[0],startpeg,endpeg]]
+        move = [disklist[0],startpeg,endpeg]
+        movelist = [move]
+        return [movelist]
 
     else:
-        #zwischenturmhoehe berechnen
+        #compute all possible pegheights for the intermediate towers
         pegheights = calculatemiddletower(startpeg,endpeg,disklist,peglist)
-        pegheight = pegheights[0]
-        print("use", pegheight)
-        #dann die movessequencelist fuer die Zwischentuerme berechnen
-        movesequencelist = []
-        #bewege einen Zwischenturm nach dem anderen
-        peglist2 = peglist.copy()
-        disklist2 = disklist.copy()
-        for peg in peglist:
-            height = pegheight[peg]
-            if height!=0:
-                movingdisks = disklist2[:height]
-                movesequencelist += movessequence(startpeg,peg,movingdisks,peglist2)
-                peglist2.remove(peg)
-                disklist2 = disklist2[height:]
+        resultlist = []
+        for pegheight in pegheights:
+            print("use", pegheight)
+            #compute movesequencelist for building the intermediate towers
+            movesequencedict = {}
+            peglist2 = peglist.copy()
+            disklist2 = disklist.copy()
+            #build one intermediate tower after the other
+            for peg in peglist:
+                height = pegheight[peg]
+                if height!=0:
+                    movingdisks = disklist2[:height]
+                    #contains all possible movessequences to move movingdisks from startpeg to peg
+                    movesequencedict[peg] = movessequence(startpeg,peg,movingdisks,peglist2)
+                    peglist2.remove(peg)
+                    disklist2 = disklist2[height:]
 
-        #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+            #construct individual movessequences from movesequencedict
+            np = 0
+            movesequenceleveldict = {} #contains all possible movessequences until this peg
+            level = 0
+            for peg in movesequencedict:
+                level += 1
+                movesequenceleveldict[level] = []
+                for addmovessequence in movesequencedict[peg]:#addmovessequence builds the intermediate tower on peg
+                    if level > 1:
+                        #lowerlevellist is a list of all movessequences that have been created before
+                        lowerlevellist = movesequenceleveldict[level-1]
+                        for oldmovessequence in lowerlevellist:#oldmovessequence is one of the movessequences that create everything before the peg
+                            newmovessequence = oldmovessequence + addmovessequence
+                            movesequenceleveldict[level].append(newmovessequence)
+                    else:#first peg, level 1
+                        movesequenceleveldict[level].append(addmovessequence)
+            finalmovessequences = movesequenceleveldict[level]#level has the max value
 
-        lastmoves = []
-        for i in range(1,len(movesequencelist)+1):
-            #iterates from len(firstmoves)-1 to 0
-            currentmove = movesequencelist[len(movesequencelist)-i]
-            #tauschen von start und endpeg
-            if currentmove[1]==startpeg:
-                peg2 = endpeg
-            elif currentmove[1]==endpeg:
-                peg2 = startpeg
-            else:
-                peg2 = currentmove[1]
-            if currentmove[2]==startpeg:
-                peg1 = endpeg
-            elif currentmove[2]==endpeg:
-                peg1 = startpeg
-            else:
-                peg1 = currentmove[2]
-            newcurrentmove = [currentmove[0],peg1,peg2]
-            lastmoves.append(newcurrentmove)
+            #construct the rest of the movessequences
 
-        return movesequencelist[:]+[[disklist[-1],startpeg,endpeg]]+lastmoves[:]
+            for movesequence in finalmovessequences:
+                lastmoves = []
+                for i in range(1,len(movesequence)+1):
+                    #iterates from len(firstmoves)-1 to 0
+                    currentmove = movesequence[len(movesequence)-i]
+                    #tauschen von start und endpeg
+                    if currentmove[1]==startpeg:
+                        peg2 = endpeg
+                    elif currentmove[1]==endpeg:
+                        peg2 = startpeg
+                    else:
+                        peg2 = currentmove[1]
+                    if currentmove[2]==startpeg:
+                        peg1 = endpeg
+                    elif currentmove[2]==endpeg:
+                        peg1 = startpeg
+                    else:
+                        peg1 = currentmove[2]
+                    newcurrentmove = [currentmove[0],peg1,peg2]
+                    lastmoves.append(newcurrentmove)
+                result = movesequence[:]+[[disklist[-1],startpeg,endpeg]]+lastmoves[:]
+                resultlist.append(result)
+        return resultlist
+
 
 def calculatemiddletower(startpeg,endpeg,disklist,peglist):
     """berechnet die Hoehe der Zwischentuerme und gibt ein dict mit dem Zwischenturm als key und der hoehe als wert aus"""
-    print("calculatemiddletower startpeg =", startpeg, ", endpeg =", endpeg, "\nuse disks", disklist, "\nuse pegs", peglist)
+    #rint("calculatemiddletower startpeg =", startpeg, ", endpeg =", endpeg, "\nuse disks", disklist, "\nuse pegs", peglist)
     m = len(peglist); As = len(disklist); mmpegdict = {}
     n = 0
     x = 1
     while As > x:
         n+=1
         x = bk(n+m-2,m-2)
-    #print(As,n)
+    #rint(As,n)
     #berechnen der minimalen und maximalen Hoehe eines Zwischenturms
     i = 0
     for peg in peglist:
@@ -107,7 +131,7 @@ def calculatemiddletower(startpeg,endpeg,disklist,peglist):
     else:
         notassigned = peglist.copy()
         notassigned.reverse()
-        #print("assignpegheights(", notassigned, pegheightdict, As, mmpegdict, ")")
+        #rint("assignpegheights(", notassigned, pegheightdict, As, mmpegdict, ")")
         pegheights = assignpegheights(notassigned, pegheightdict, As, mmpegdict)
     return pegheights
 
@@ -124,7 +148,7 @@ def assignpegheights(notassigned, pegheightdict, totaldisks, mmpegdict):
     for peg in pegheightdict:
         disknumber += pegheightdict[peg]
     remaining = totaldisks - disknumber -1
-    #print(remaining, "=", totaldisks, "-", disknumber)
+    #rint(remaining, "=", totaldisks, "-", disknumber)
 
     if len(notassigned) == 0 and remaining == 0:
         #all disks are assigned to a peg
@@ -146,10 +170,10 @@ def assignpegheights(notassigned, pegheightdict, totaldisks, mmpegdict):
         pegheights = []
         lower = max(minimum, remaining-maxsum)
         upper = min(maximum, remaining-minsum)
-        #print(space, "peg", peg, "remaining", remaining, pegheightdict)
-        #print(space, "range:", lower, upper)
+        #rint(space, "peg", peg, "remaining", remaining, pegheightdict)
+        #rint(space, "range:", lower, upper)
         for pegheight in range(lower, upper+1):
-            #print(space, peg, " has pegheight", pegheight, "while remaining is", remaining)
+            #rint(space, peg, " has pegheight", pegheight, "while remaining is", remaining)
             if remaining-pegheight >= 0:
                 pegheightdict[peg] = pegheight
                 a = assignpegheights(notassigned.copy(), copy.deepcopy(pegheightdict), totaldisks, mmpegdict)
@@ -166,9 +190,5 @@ def movessequence_ui(n,k):
     ST = TH(n,k)
     configurations[0] = [ST]
     moves = movessequence(peglist[0],peglist[-1],disklist,peglist)
-    #print(moves, len(moves))
+    #rint(moves, len(moves))
     return moves
-
-
-
-print(movessequence_ui(5,5))
--- results.py
+++ results.py
@@ -17,15 +17,54 @@ def comparevalues(nmax, kmax):
             print(adjustedUpsilon(n,k), end = "|")
         print("")
 
+def comparehs(THs, show = True):
+    """
+    compare the history of a list of THobjects
+    !important: only pass THobject with same length of history
+    """
+    M = len(THs[0].history)
+    if show:
+        historylist = []
+        movedict = {}
+        for m in range(0,M):
+            movedict[m] = []
+        for th in THs:
+            movecounter = -1
+            for config in th.history:
+                movecounter += 1
+                movedict[movecounter].append(normalize(th.disks, th.pegs, config))
+        #movedict[n] contains the normalizedconfigurations of all THobjects in THs after n moves
+        pprint.pprint(movedict)
+    different = {}
+    for th1 in THs:
+        for th2 in THs:
+            if th1 != th2:
+                for movenumber in range(M):
+                    th1config = normalize(th1.disks, th1.pegs, th1.history[movenumber])
+                    th2config = normalize(th2.disks, th2.pegs, th2.history[movenumber])
+                    try:
+                        value = different[th1][th2]
+                    except:
+                        #only the first time
+                        different[th1] = {}
+                        different[th1][th2] = 0
+                    if th1config != th2config:
+                        different[th1][th2] = 1
+    return different
 
 if __name__ == "__main__":
     disks = int(sys.argv[1])
     pegs = int(sys.argv[2])
-    moves = movessequence_ui(disks, pegs)
-    ST = TH(disks, pegs)
-    print(moves)
-    for movelist in moves:
-        ST.move(movelist)
+    movepossibilities = movessequence_ui(disks, pegs)
+    #print(moves)
+    thlist = []
+    for movelist in movepossibilities:
+        ST = TH(disks, pegs)
+        thlist.append(ST)
+        for move1 in movelist:
+            ST.move(move1)
+    different = comparehs(thlist, show = True)
+    print(different)
     #print(ST.history)
     #visualize(('list', ST.history))
     #success_instances = bruteforce(disks, pegs)
